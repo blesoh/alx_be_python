e6be0729108ac727fe1b16aec32e88a9a57986ff
blesoh <rapandoblessing@gmail.com>
2025-09-11 05:59:53 -0600

    Fix spelling on interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,7 +2,7 @@ principal = 1000 # representing $1000
 rate = 0.05 # representing 5% annual interest 
 time = 3 # representing 3 years
 # Simple Interest Calculation
-Intrest = principal * rate * time
+Interest = principal * rate * time
 # Result
-print("The Simple Interest is:", Intrest)
+print("The Simple Interest is:", Interest)
 
