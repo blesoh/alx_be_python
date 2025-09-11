6e0c8a53f1401e047deb16a03d8c0bfa11e629ef
blesoh <rapandoblessing@gmail.com>
2025-09-11 05:28:08 -0600

    Fix: place basic_operations.py inside python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+# Addition
+sum_result = number1 + number2
+# Subtraction
+difference_result = number1 - number2
+# Multiplication
+product_result = number1 * number2
+# Results
+print("Addition of 10 and 5 is", sum_result)
+print("Subtraction of 10 and 5 is", difference_result)  
+print("Multiplication of 10 and 5 is", product_result)
