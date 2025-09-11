141902dbcf9b15a51f0de99ea070a76c4f390bf4
blesoh <rapandoblessing@gmail.com>
2025-09-11 05:49:54 -0600

    Simple intrest file

---
+++ python_introduction/simple_intrest.py
@@ -0,0 +1,8 @@
+principal = 1000 # representing $1000
+rate = 0.05 # representing 5% annual interest 
+time = 3 # representing 3 years
+# Simple Interest Calculation
+Intrest = principal * rate * time
+# Result
+print("The Simple Interest is:", Intrest)
+
