12e4e425916f9fad270158a2394cadd4ae90e8d0
blesoh <rapandoblessing@gmail.com>
2025-09-19 19:41:36 +0300

    remove current_weather add weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,10 +6,10 @@ weather = input("What's the weather like today? (sunny/rainy/cold):")
 if weather == "Sunny":        
     print("Wear a t-shirt and sunglasses.")
 
-elif current_weather == "Rainy":
+elif weather == "Rainy":
     print("Don't forget your umbrella and a raincoat.")
 
-elif current_weather == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 
 else:
