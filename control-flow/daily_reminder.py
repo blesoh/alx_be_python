d938a429319698eed7967959c16d7956f59a7af8
blesoh <rapandoblessing@gmail.com>
2025-09-21 14:45:10 +0300

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,25 +4,25 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-# Match case for priority
+# Match case for priority and  If statement to modify reminder
+
 match priority:
     case "high":
-        reminder = f" '{task}' is a high priority task."
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task and requires immediate attention today!")
+        else:
+            print(f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time.")
     case "medium":
-        reminder = f" '{task}' is a medium priority task."
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a medium priority task and requires immediate attention today!")
+        else:   
+            print(f"Reminder: '{task}' is a medium priority task. This task is not time-bound, you can schedule it more flexibly.")
     case "low":
-        reminder = f" '{task}' is a low priority task."
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a low priority task and requires immediate attention today!") 
+        else:   
+            print(f"Reminder: '{task}' is a low priority task. This task is not time-bound, you can schedule it more flexibly.")
     case _:
-        reminder = f"'{task}' has an unknown priority."
+        print(f"'{task}' has an unknown priority.")
 
-# If statement to modify reminder
-if time_bound == "yes":
-    reminder = reminder + " It requires immediate attention today!"
-else:
-    if priority == "low":
-        reminder = reminder + " Consider completing it when you have free time."
-    else:
-        reminder = reminder + " This task is not time-bound, you can schedule it more flexibly."
 
-# Final output
-print(reminder)
